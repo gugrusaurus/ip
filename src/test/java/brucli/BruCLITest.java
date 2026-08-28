8ae997289cbc5f013bd747438e5d09f933e643bb
gugrusaurus <gugrusaurus@gmail.com>
2026-08-28 13:59:11 +0800

    Add JUnit and Jar

---
+++ src/test/java/brucli/BruCLITest.java
@@ -0,0 +1,17 @@
+package brucli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BruCLITest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest() {
+        assertEquals(4, 4);
+    }
+}
